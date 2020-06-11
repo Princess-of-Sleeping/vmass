3bc02c8e8f914dfa2b8e73f40fa916da9ce90f08
Princess of Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2020-06-11 15:05:24 +0900

    Optimize code

--- src/vmass.c
+++ src/vmass.c
@@ -29,41 +29,64 @@ const char umass_start_byepass_patch[] = {
 	0x00, 0x20, 0x00, 0x00
 };
 
-#define VMASS_PAGE_SIZE 0x100000
-#define VMASS_PAGE_NUM  32
+#define SIZE_2MiB   0x200000
+#define SIZE_4MiB   0x400000
+#define SIZE_10MiB  0xA00000
+#define SIZE_16MiB 0x1000000
+
+SceSize g_vmass_size;
+
+typedef struct VmassPageAllocInfo {
+	unsigned int mem_type;
+	unsigned int paddr;
+	SceSize size;
+} VmassPageAllocInfo;
+
+const VmassPageAllocInfo page_alloc_list[] = {
+	// ScePhyMemPartGameCdram
+	{0x40408006, 0x00000000, SIZE_10MiB},
+
+	// ScePhyMemPartPhyCont
+	{0x30808006, 0x00000000, SIZE_4MiB},
+
+	// SceDisplay
+	{0x10208006, 0x1C000000, SIZE_2MiB}
+};
+
+#define VMASS_PAGE_NUM (sizeof(page_alloc_list) / sizeof(VmassPageAllocInfo))
 
 void *membase[VMASS_PAGE_NUM];
 
 SceKernelLwMutexWork lw_mtx;
-SceUsbMassDevInfo dev_info;
 
-int vmassGetDevInfo(SceUsbMassDevInfo *info){
+int _vmassGetDevInfo(SceUsbMassDevInfo *pInfo){
 
-	ksceKernelLockFastMutex(&lw_mtx);
+	if(pInfo == NULL)
+		return -1;
 
-	memcpy(info, &dev_info, sizeof(*info));
-
-	ksceKernelUnlockFastMutex(&lw_mtx);
+	pInfo->sector_size          = 0x200;
+	pInfo->data_04              = 0;
+	pInfo->number_of_all_sector = g_vmass_size >> 9;
+	pInfo->data_0C              = 0;
 
 	return 0;
 }
 
-int vmassReadSector(SceSize sector_pos, void *data, SceSize sector_num){
-
-	int res = 0;
+int _vmassReadSector(SceSize sector_pos, void *data, SceSize sector_num){
 
 	int page_idx = 0;
 	SceSize off = (sector_pos << 9), size = (sector_num << 9), work_size;
 
-	ksceKernelLockFastMutex(&lw_mtx);
+	if((size == 0) || ((off + size) > g_vmass_size) || (off >= g_vmass_size) || (size > g_vmass_size))
+		return -1;
 
-	while(off >= VMASS_PAGE_SIZE){
+	while(off >= page_alloc_list[page_idx].size){
+		off -= page_alloc_list[page_idx].size;
 		page_idx++;
-		off -= VMASS_PAGE_SIZE;
 	}
 
 	if(off != 0){
-		work_size = VMASS_PAGE_SIZE - off;
+		work_size = page_alloc_list[page_idx].size - off;
 		if(work_size > size)
 			work_size = size;
 
@@ -75,7 +98,7 @@ int vmassReadSector(SceSize sector_pos, void *data, SceSize sector_num){
 	}
 
 	while(size != 0){
-		work_size = (size > VMASS_PAGE_SIZE) ? VMASS_PAGE_SIZE : size;
+		work_size = (size > page_alloc_list[page_idx].size) ? page_alloc_list[page_idx].size : size;
 
 		memcpy(data, membase[page_idx], work_size);
 		size -= work_size;
@@ -83,27 +106,24 @@ int vmassReadSector(SceSize sector_pos, void *data, SceSize sector_num){
 		page_idx++;
 	}
 
-	ksceKernelUnlockFastMutex(&lw_mtx);
-
-	return res;
+	return 0;
 }
 
-int vmassWriteSector(SceSize sector_pos, const void *data, SceSize sector_num){
-
-	int res = 0;
+int _vmassWriteSector(SceSize sector_pos, const void *data, SceSize sector_num){
 
 	int page_idx = 0;
 	SceSize off = (sector_pos << 9), size = (sector_num << 9), work_size;
 
-	ksceKernelLockFastMutex(&lw_mtx);
+	if((size == 0) || ((off + size) > g_vmass_size) || (off >= g_vmass_size) || (size > g_vmass_size))
+		return -1;
 
-	while(off >= VMASS_PAGE_SIZE){
+	while(off >= page_alloc_list[page_idx].size){
+		off -= page_alloc_list[page_idx].size;
 		page_idx++;
-		off -= VMASS_PAGE_SIZE;
 	}
 
 	if(off != 0){
-		work_size = VMASS_PAGE_SIZE - off;
+		work_size = page_alloc_list[page_idx].size - off;
 		if(work_size > size)
 			work_size = size;
 
@@ -115,13 +135,50 @@ int vmassWriteSector(SceSize sector_pos, const void *data, SceSize sector_num){
 	}
 
 	while(size != 0){
-		work_size = (size > VMASS_PAGE_SIZE) ? VMASS_PAGE_SIZE : size;
+		work_size = (size > page_alloc_list[page_idx].size) ? page_alloc_list[page_idx].size : size;
 
 		memcpy(membase[page_idx], data, work_size);
 		size -= work_size;
 		data += work_size;
 		page_idx++;
 	}
+
+	return 0;
+}
+
+int vmassGetDevInfo(SceUsbMassDevInfo *info){
+
+	int res;
+
+	ksceKernelLockFastMutex(&lw_mtx);
+
+	res = _vmassGetDevInfo(info);
+
+	ksceKernelUnlockFastMutex(&lw_mtx);
+
+	return res;
+}
+
+int vmassReadSector(SceSize sector_pos, void *data, SceSize sector_num){
+
+	int res;
+
+	ksceKernelLockFastMutex(&lw_mtx);
+
+	res = _vmassReadSector(sector_pos, data, sector_num);
+
+	ksceKernelUnlockFastMutex(&lw_mtx);
+
+	return res;
+}
+
+int vmassWriteSector(SceSize sector_pos, const void *data, SceSize sector_num){
+
+	int res;
+
+	ksceKernelLockFastMutex(&lw_mtx);
+
+	res = _vmassWriteSector(sector_pos, data, sector_num);
 
 	ksceKernelUnlockFastMutex(&lw_mtx);
 
@@ -131,55 +188,93 @@ int vmassWriteSector(SceSize sector_pos, const void *data, SceSize sector_num){
 // Load umass.skprx to avoid taiHEN's unlinked problem
 int load_umass(void){
 
+	int res;
 	SceUID umass_modid, patch_uid;
 
 	// Since bootimage cannot be mounted after the second time, load vitashell's umass.skprx
 	umass_modid = ksceKernelLoadModule("ux0:VitaShell/module/umass.skprx", 0, NULL);
+	if(umass_modid < 0)
+		return umass_modid;
 
 	patch_uid = taiInjectDataForKernel(0x10005, umass_modid, 0, 0x1546, umass_start_byepass_patch, sizeof(umass_start_byepass_patch) - 2);
 
 	int start_res = -1;
-	ksceKernelStartModule(umass_modid, 0, NULL, 0, NULL, &start_res);
+	res = ksceKernelStartModule(umass_modid, 0, NULL, 0, NULL, &start_res);
+	if(res >= 0)
+		res = start_res;
 
 	taiInjectReleaseForKernel(patch_uid);
 
-	return 0;
+	return res;
 }
 
-int vmassInit(void){
+int vmassAllocStoragePage(void){
 
 	SceUID memid;
-	int buf[0x200 >> 2];
-	FatHeader fat_header;
+	SceKernelAllocMemBlockKernelOpt opt, *pOpt;
 
 	for(int i=0;i<VMASS_PAGE_NUM;i++){
 
-		memid = ksceKernelAllocMemBlock("VmassStoragePage", 0x40404006, VMASS_PAGE_SIZE, NULL);
+		pOpt = NULL;
+
+		if(page_alloc_list[i].paddr != 0){
+			memset(&opt, 0, sizeof(opt));
+			opt.size = sizeof(opt);
+			opt.attr = SCE_KERNEL_ALLOC_MEMBLOCK_ATTR_HAS_PADDR;
+			opt.paddr = page_alloc_list[i].paddr;
+			pOpt = &opt;
+		}
+
+		memid = ksceKernelAllocMemBlock("VmassStoragePage", page_alloc_list[i].mem_type, page_alloc_list[i].size, pOpt);
 		if(memid < 0){
 
 			ksceDebugPrintf("failed 0x%X\n", i);
 
+			i = VMASS_PAGE_NUM;
+
 			do {
 				i--;
-				ksceKernelFreeMemBlock(ksceKernelFindMemBlockByAddr(membase[i], 0));
+				if(membase[i] != NULL)
+					ksceKernelFreeMemBlock(ksceKernelFindMemBlockByAddr(membase[i], 0));
 			} while(i != 0);
 
 			return memid;
 		}
 
 		ksceKernelGetMemBlockBase(memid, &membase[i]);
+
+		g_vmass_size += page_alloc_list[i].size;
 	}
 
-	memset(&dev_info, 0, sizeof(dev_info));
+	return 0;
+}
 
-	dev_info.sector_size = 0x200;
-	dev_info.number_of_all_sector = (VMASS_PAGE_SIZE * VMASS_PAGE_NUM) >> 9;
+int vmassInit(void){
+
+	int res, buf[0x200 >> 2];
+	FatHeader fat_header;
+
+	res = ksceKernelInitializeFastMutex(&lw_mtx, "VmassMutex", 0, 0);
+	if(res < 0)
+		return res;
+
+	res = vmassAllocStoragePage();
+	if(res < 0)
+		return res;
 
 	memset(buf, 0, 0x200);
 
-	buf[0] = 0xFFFFFFF8;
+	if(g_vmass_size >= SIZE_16MiB){
 
-	setFat16Header(&fat_header, dev_info.number_of_all_sector);
+		buf[0] = 0xFFFFFFF8;
+
+		setFat16Header(&fat_header, g_vmass_size >> 9);
+	}else{
+
+		buf[0] = 0xFFFFF8;
+
+		setFat12Header(&fat_header, g_vmass_size >> 9);
+	}
 
 	vmassWriteSector(0, &fat_header, 1);
 
@@ -187,9 +282,7 @@ int vmassInit(void){
 	vmassWriteSector(2, buf, 1);
 	vmassWriteSector(fat_header.fat_base.fat_size_16 + 2, buf, 1);
 
-	ksceKernelInitializeFastMutex(&lw_mtx, "VmassMtx", 0, 0);
+	res = load_umass();
 
-	load_umass();
-
-	return 0;
+	return res;
 }
